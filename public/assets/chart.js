ad8d2faecc2cb006f5af78f77a999b3b471217c5
Whitney <bdwseybold@gmail.com>
2016-11-04 18:01:22 -0500

    took out angular chart

---
+++ public/assets/chart.js
@@ -0,0 +1,58 @@
+angular.module('app', ['chart.js']);
+
+angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+});
+
+
+
+
+//runners information chart compilation -->
+
+// { meta { timestamp: date,
+//          location: {
+//            long: #;
+//            lat: #}
+//         },
+// data: [{
+//         timestamp: date,
+//         location: {long: #, lat: #}
+//         elevation: #,
+//         heartrate: #,
+//       },
+//       ...include more objects and use for loop to create a new timestamp every ten seconds (i * 10)
+//     ],
+// runner: {
+//   dob:
+//   height:
+//   weight:
+// }
+// }
---
+++ server.js
@@ -0,0 +1,12 @@
+// angular.module('myApp', ['dark-sky']);
+//
+// app.config(['darkSkyProvider', function(darkSkyProvider) {
+//     darkSkyProvider
+//         .setApiKey('2b16fdb93fb0f82bded5f1bddf47c1c7');
+// }]);
+//
+
+
+
+
+// "https://api.darksky.net/forecast/2b16fdb93fb0f82bded5f1bddf47c1c7/37.8267,-122.4233"
