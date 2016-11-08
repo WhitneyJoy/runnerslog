eb06b6fa2bdddeec8fe57c64e6424f7bf186e480
Whitney <bdwseybold@gmail.com>
2016-11-08 15:06:10 -0600

    combined chart and app

--- public/assets/app.js
+++ public/assets/app.js
@@ -27,3 +27,35 @@ angular.module('editableform', [])
 //     $scope.myData = response;
 //   });
 // });
+//chart
+angular.module("app", ["chart.js"]).controller("LineCtrl", function ($scope) {
+
+  $scope.labels = ["1 mile", "2 miles", "3 miles", "4 miles", "5 miles", "6 miles", "7 miles"];
+  $scope.series = ['Miles', 'Time'];
+  $scope.data = [
+    [5, 3, 6, 4, 4.5, 3.6, 7],
+    [30, 20, 45, 35, 38, 27, 57]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
+});
