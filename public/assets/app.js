32486fed056c5801fb84c9130487e23f3ca6eca4
Whitney <bdwseybold@gmail.com>
2016-11-08 15:12:13 -0600

    dropdown

--- public/assets/app.js
+++ public/assets/app.js
@@ -11,14 +11,14 @@ angular.module('editableform', [])
 
 
 //dropdown
-// var app = angular.module('app', []);
-// app.controller('myCtrl', function($scope) {
-//     $scope.cars = {
-//         car01 : {brand : "Ford", model : "Mustang", color : "red"},
-//         car02 : {brand : "Fiat", model : "500", color : "white"},
-//         car03 : {brand : "Volvo", model : "XC90", color : "black"}
-//     }
-// });
+var app = angular.module('app', []);
+app.controller('myCtrl', function($scope) {
+    $scope.cars = {
+        car01 : {brand : "Ford", model : "Mustang", color : "red"},
+        car02 : {brand : "Fiat", model : "500", color : "white"},
+        car03 : {brand : "Volvo", model : "XC90", color : "black"}
+    }
+});
 
 //runners json information to display
 // myApp=angular.module('app', [])
